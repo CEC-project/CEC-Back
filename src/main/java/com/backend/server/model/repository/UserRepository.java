6866df6acb3b5f6111a33591f657f0b9aa8c3fb9
qkr10 <qkrtngks10@naver.com>
2025-04-15 01:13:16 +0900

    feat : (관리자 웹) 사용자 관리 - 목록 조회 기능 구현
    1. JPA Specification 을 이용한 동적 쿼리 구현
    2. ServerApplication.java 파일의 JPA Auditing 관련 오류도 수정

--- src/main/java/com/backend/server/model/repository/UserRepository.java
+++ src/main/java/com/backend/server/model/repository/UserRepository.java
@@ -5,7 +5,8 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.backend.server.model.entity.User;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
     Optional<User> findByStudentNumber(String studentNumber);
 }
