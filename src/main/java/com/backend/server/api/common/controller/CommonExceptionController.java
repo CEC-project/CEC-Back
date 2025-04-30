9c5ab40a85efa78a733b0733f8c77581494ea821
qkr10 <qkrtngks10@naver.com>
2025-04-30 14:25:22 +0900

    fix : 에러 로그 출력 및 DB 예외 추가

--- src/main/java/com/backend/server/api/common/controller/CommonExceptionController.java
+++ src/main/java/com/backend/server/api/common/controller/CommonExceptionController.java
@@ -1,7 +1,10 @@
 package com.backend.server.api.common.controller;
 
 import com.backend.server.api.common.dto.ApiResponse;
+import jakarta.persistence.PersistenceException;
 import jakarta.servlet.http.HttpServletResponse;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,13 +16,28 @@ public class CommonExceptionController {
     @ExceptionHandler(RuntimeException.class)
     public ApiResponse<Object> handleRuntimeException(HttpServletResponse res, RuntimeException ex) {
         res.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-        return ApiResponse.fail(ex.getMessage());
+        log.error(ex.getMessage(), ex);
+        return ApiResponse.fail(getTimeString() + ex.getMessage());
+    }
+
+    @ExceptionHandler(PersistenceException.class)
+    public ApiResponse<Object> handlePersistenceException(HttpServletResponse res, PersistenceException ex) {
+        res.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        log.error(ex.getMessage(), ex);
+        return ApiResponse.fail(getTimeString() + "DB가 sql 을 처리하는중 오류가 발생했습니다.");
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ApiResponse<Object> handleIllegalArgumentException(HttpServletResponse res, IllegalArgumentException ex) {
         res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-        return ApiResponse.fail(ex.getMessage());
+        log.error(ex.getMessage(), ex);
+        return ApiResponse.fail(getTimeString() + ex.getMessage());
+    }
+
+    private String getTimeString() {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX --- ");
+        return sdf.format(cal.getTime());
     }
 
 }
