6fe164890137338764943f00160a806e8eee5e1b
qkr10 <qkrtngks10@naver.com>
2025-05-01 10:10:15 +0900

    fix : 로그인 응답 오타 수정 (#48)

--- src/main/java/com/backend/server/api/common/dto/auth/CommonSignInResponse.java
+++ src/main/java/com/backend/server/api/common/dto/auth/CommonSignInResponse.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @Builder
 public class CommonSignInResponse {
     private Long id;
-    private String studentNubmer;
+    private String studentNumber;
     private String nickname;
     private String role;
     private String department;
@@ -22,7 +22,7 @@ public class CommonSignInResponse {
 
     public CommonSignInResponse(User user, String access) {
         this.id = user.getId();
-        this.studentNubmer = user.getStudentNumber();
+        this.studentNumber = user.getStudentNumber();
         this.nickname = user.getNickname();
         this.role = user.getRole().name();
         this.department = user.getDepartment();
