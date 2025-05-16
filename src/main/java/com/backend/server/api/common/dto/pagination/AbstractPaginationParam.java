f77c4a208340a92760e31a4dba078fc9524e653b
klaus9267 <klaus9267@gmail.com>
2025-05-16 21:47:14 +0900

    docs : swagger 추가

--- src/main/java/com/backend/server/api/common/dto/pagination/AbstractPaginationParam.java
+++ src/main/java/com/backend/server/api/common/dto/pagination/AbstractPaginationParam.java
@@ -1,10 +1,10 @@
 package com.backend.server.api.common.dto.pagination;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.security.core.parameters.P;
 
 /**
  * {@code AbstractPaginationParam}은 페이지네이션을 위한 추상 클래스입니다.
@@ -12,12 +12,19 @@ import org.springframework.security.core.parameters.P;
  */
 @Getter
 public abstract class AbstractPaginationParam {
-  protected Integer page = 0;
-  protected Integer size = 10;
-  private PaginationSortType field = PaginationSortType.EMPTY;
-  protected Sort.Direction direction = Sort.Direction.DESC;
+  @Schema(description = "페이지 번호 (기본값 0)", example = "0")
+  protected Integer page;
+
+  @Schema(description = "페이지당 크기 (기본값 = 10)", example = "10")
+  protected Integer size;
+
+  @Schema(description = "정렬 기준(기본값 = ID)", example = "ID")
+  protected PaginationSortType sortBy;
+
+  @Schema(description = "정렬 방법(기본값 = DESC)", example = "DESC")
+  protected Sort.Direction direction;
 
   public Pageable toPageable() {
-    return PageRequest.of(page, size, direction, field.getField());
+    return PageRequest.of(page, size, direction, sortBy.getField());
   }
 }
