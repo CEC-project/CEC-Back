557a30a9c54c5441b615e536bfa9f6d5935a4e6d
qkr10 <qkrtngks10@naver.com>
2025-05-18 10:16:23 +0900

    fix : user 테이블 최신에 맞게 변경

--- sql/dml/user-insert-3.sql
+++ sql/dml/user-insert-3.sql
@@ -1,26 +1,20 @@
 -- 아래 계정들의 비밀번호는 asdf1234! 임
 
-INSERT INTO users (
-    name, student_number, nickname, department, major, grade, "group", gender, professor, phone_number,
-    email, password, profile_picture, birth_date, rental_count, damage_count,
-    restriction_count, report_count, role, equipment_id, bookmark_id, complaint_id,
-    rental_history_id, created_at, updated_at
-) VALUES
-      (
-          '슈퍼어드민', '000', '길동이', '학과', '세부전공', 3, 'A조', '남', '김교수', '010-1234-5678',
-          'hong@example.com', '$2a$10$2/ZUDynpz0BDpGUt/FLJ.OumPW6INKT.Hotj65QofSY5896MQmzW2', 'profile1.jpg', '2001-03-15', 5, 0,
-          1, 2, 2, 'EQ-001', 'BM-001', 'CP-001',
-          'RH-001', NOW(), NOW()
-      ),
-      (
-          '김영희', '20230002', '김영희', '학과', '세부전공', 2, 'B조', '여', '이교수', '010-2345-6789',
-          'kim@example.com', '$2a$10$2/ZUDynpz0BDpGUt/FLJ.OumPW6INKT.Hotj65QofSY5896MQmzW2', NULL, '2002-06-22', 2, 1,
-          0, 1, 0, 'EQ-002', 'BM-002', 'CP-002',
-          'RH-002', NOW(), NOW()
-      ),
-      (
-          '박철수', '20230003', '철수짱', '학과', '세부전공', 4, 'A조', '남', '최교수', '010-3456-7890',
-          'park@example.com', '$2a$10$2/ZUDynpz0BDpGUt/FLJ.OumPW6INKT.Hotj65QofSY5896MQmzW2', 'profile3.png', '2000-12-05', 10, 3,
-          2, 5, 1, 'EQ-003', 'BM-003', 'CP-003',
-          'RH-003', NOW(), NOW()
-      );
+insert into public.users (birth_date, damage_count, grade, rental_count, report_count, restriction_count, created_at,
+                          professor_id, updated_at, user_id, department, email, gender, "group", major, name, nickname,
+                          password, phone_number, profile_picture, role, student_number)
+values  ('2001-03-15', 0, 3, 5, 2, 1,
+         '2025-05-16 13:40:26.060319', null, '2025-05-16 13:40:26.060319',
+         1, '학과', 'hong@example.com', '남', 'A조', '세부전공',
+         '슈퍼어드민', '길동이', '$2a$10$2/ZUDynpz0BDpGUt/FLJ.OumPW6INKT.Hotj65QofSY5896MQmzW2',
+         '010-1234-5678', 'profile1.jpg', 'ROLE_ADMIN', '000'),
+        ('2002-06-22', 1, 2, 2, 1, 0,
+         '2025-05-16 13:40:26.060319', null, '2025-05-16 13:40:26.060319',
+         2, '학과', 'kim@example.com', '여', 'B조', '세부전공',
+         '김영희', '김영희', '$2a$10$2/ZUDynpz0BDpGUt/FLJ.OumPW6INKT.Hotj65QofSY5896MQmzW2',
+         '010-2345-6789', null, 'ROLE_ADMIN', '20230002'),
+        ('2000-12-05', 3, 4, 10, 5, 2,
+         '2025-05-16 13:40:26.060319', null, '2025-05-16 13:40:26.060319',
+         3, '학과', 'park@example.com', '남', 'A조', '세부전공',
+         '박철수', '철수짱', '$2a$10$2/ZUDynpz0BDpGUt/FLJ.OumPW6INKT.Hotj65QofSY5896MQmzW2',
+         '010-3456-7890', 'profile3.png', 'ROLE_SUPER_ADMIN', '202300003');
