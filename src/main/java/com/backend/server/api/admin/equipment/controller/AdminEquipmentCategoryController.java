d1de7c0867f17a4efd63ea3adbb894002b72251b
geniusBrainLsm <87559232+geniusBrainLsm@users.noreply.github.com>
2025-05-20 16:14:28 +0900

    fix : 장비 API (#106)

--- src/main/java/com/backend/server/api/admin/equipment/controller/AdminEquipmentCategoryController.java
+++ src/main/java/com/backend/server/api/admin/equipment/controller/AdminEquipmentCategoryController.java
@@ -14,7 +14,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
