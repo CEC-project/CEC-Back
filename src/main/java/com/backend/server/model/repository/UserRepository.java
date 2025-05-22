b815bc519656ac8a373246ff1331788d3e56f0c7
qkr10 <qkrtngks10@naver.com>
2025-05-22 19:10:59 +0900

    fix : n+1 문제 해결을 위해 entitygraph 사용

--- src/main/java/com/backend/server/model/repository/UserRepository.java
+++ src/main/java/com/backend/server/model/repository/UserRepository.java
@@ -3,6 +3,10 @@ package com.backend.server.model.repository;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,6 +17,11 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+
+    // 유저 페이지네이션시 N+1 방지
+    @EntityGraph(attributePaths = {"professor"})
+    Page<User> findAll(Specification<User> spec, Pageable pageable);
+
     Optional<User> findByEmail(String email);
     Optional<User> findByStudentNumber(String studentNumber);
     List<User> findByRole(Role role);
