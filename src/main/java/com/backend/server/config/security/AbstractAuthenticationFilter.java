debb194a7a15fc4f7a439cde367789161a356857
qkr10 <qkrtngks10@naver.com>
2025-05-22 22:14:03 +0900

    feat : prod 프로필이 아니면, 로그인 없이 요청 가능

---
+++ src/main/java/com/backend/server/config/security/AbstractAuthenticationFilter.java
@@ -0,0 +1,7 @@
+package com.backend.server.config.security;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public abstract class AbstractAuthenticationFilter extends OncePerRequestFilter {
+
+}
