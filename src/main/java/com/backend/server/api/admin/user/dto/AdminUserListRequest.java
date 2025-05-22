c9974030da1220b8ffd11dacedbbd726904606a0
qkr10 <qkrtngks10@naver.com>
2025-05-22 15:56:06 +0900

    fix : 사용자 조회 api 설명 수정 (#119)

--- src/main/java/com/backend/server/api/admin/user/dto/AdminUserListRequest.java
+++ src/main/java/com/backend/server/api/admin/user/dto/AdminUserListRequest.java
@@ -10,9 +10,9 @@ public class AdminUserListRequest {
     private int size = 10;
     private String searchKeyword;
     private Integer searchType; // 검색 유형 (0:name|1:phoneNumber|2:studentNumber)
-    private String year;
-    private String gender;
-    private String professor;
+    private Integer grade; // 1 or 2 or 3 or 4 or null
+    private String gender; // '남' or '여' or null
+    private Long professorId; // 교수 테이블의 id or null
     private Integer sortBy = 0; // 정렬 기준 (0:name|1:studentNumber|2:restrictionCount)
     private String sortDirection = "asc"; // asc, desc
 }
