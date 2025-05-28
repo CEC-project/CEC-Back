de800aff3d151ce0b3e4e34645b9cf693a934ef9
qkr10 <qkrtngks10@naver.com>
2025-05-28 10:09:08 +0900

    fix : 조회용 dto 수정 (#177)

--- src/main/java/com/backend/server/api/admin/classroom/dto/AdminClassroomSearchRequest.java
+++ src/main/java/com/backend/server/api/admin/classroom/dto/AdminClassroomSearchRequest.java
@@ -1,6 +1,5 @@
 package com.backend.server.api.admin.classroom.dto;
 
-import com.backend.server.config.annotation.EnumValidator;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,10 +12,5 @@ public class AdminClassroomSearchRequest {
 
     private String keyword;
 
-    @EnumValidator(enumClass = SearchType.class, message = "검색타입은 (ID, NAME, DESCRIPTION, ALL) 중 하나입니다.")
-    private String type;
-
-    public SearchType getSearchType() {
-        return SearchType.valueOf(type);
-    }
+    private SearchType type;
 }
