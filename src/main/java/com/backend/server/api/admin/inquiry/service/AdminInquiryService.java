0e0965dc0d15882fbf1b4a33522b91fe48706dbe
qkr10 <qkrtngks10@naver.com>
2025-05-28 11:03:01 +0900

    feat : [관리자 웹] [문의 답변 등록] 구현 (#179)

--- src/main/java/com/backend/server/api/admin/inquiry/service/AdminInquiryService.java
+++ src/main/java/com/backend/server/api/admin/inquiry/service/AdminInquiryService.java
@@ -1,5 +1,6 @@
 package com.backend.server.api.admin.inquiry.service;
 
+import com.backend.server.api.admin.inquiry.dto.AdminInquiryAnswerRequest;
 import com.backend.server.api.admin.inquiry.dto.AdminInquiryListRequest;
 import com.backend.server.api.admin.inquiry.dto.AdminInquiryListResponse;
 import com.backend.server.api.admin.inquiry.dto.AdminInquirySortType;
@@ -10,6 +11,7 @@ import com.backend.server.model.entity.User;
 import com.backend.server.model.repository.InquiryAnswerRepository;
 import com.backend.server.model.repository.InquiryRepository;
 import com.backend.server.model.repository.InquirySpecification;
+import com.backend.server.model.repository.UserRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -24,6 +26,7 @@ public class AdminInquiryService {
 
     private final InquiryRepository inquiryRepository;
     private final InquiryAnswerRepository answerRepository;
+    private final UserRepository userRepository;
 
     @Transactional(readOnly = true)
     public AdminInquiryListResponse getInquiries(AdminInquiryListRequest request) {
@@ -41,5 +44,15 @@ public class AdminInquiryService {
         List<Professor> professors = users.stream().map(User::getProfessor).toList();
 
         return new AdminInquiryListResponse(inquiries, answers, responders, users, professors);
+    }
+
+    public Long addResponse(Long inquiryId, AdminInquiryAnswerRequest request, Long responderId) {
+        User responder = userRepository.findById(responderId).orElseThrow(() ->
+                new IllegalArgumentException("현재 로그인한 사용자가 DB 에서 찾아지지 않음"));
+        Inquiry inquiry = inquiryRepository.findById(inquiryId).orElseThrow(() ->
+                new IllegalArgumentException("답변할 문의의 id가 DB 에서 찾아지지 않음"));
+        InquiryAnswer answer = request.toEntity(responder, inquiry);
+        answerRepository.save(answer);
+        return answer.getId();
     }
 }
