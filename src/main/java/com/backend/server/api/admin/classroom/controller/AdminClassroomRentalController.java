b2a486a14a996f05964bad03a5ad923222dbc07c
qkr10 <qkrtngks10@naver.com>
2025-05-30 20:24:55 +0900

    fix : 강의실 대여 관련 관리자 api 통합 (#205)

--- src/main/java/com/backend/server/api/admin/classroom/controller/AdminClassroomRentalController.java
+++ src/main/java/com/backend/server/api/admin/classroom/controller/AdminClassroomRentalController.java
@@ -13,7 +13,6 @@ import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -68,6 +67,7 @@ public class AdminClassroomRentalController {
             summary = "강의실 승인 취소시킬 목록 조회 API",
             description = """
             **사용자가 대여 요청 날린 시간 기준 정렬**
+            **대여 시작시간이 아직 안된 것만 뜹니다.**
             
             - **요청 바디**
               - **keyword** : 검색 키워드 (nullable, 예: "컴퓨터실")
@@ -83,50 +83,16 @@ public class AdminClassroomRentalController {
         return ApiResponse.success("강의실 승인 취소시킬 목록 조회 성공", result);
     }
 
-    @Operation(summary = "강의실 대여 승인 API")
-    @PatchMapping("/{classroomId}/accept")
-    public ApiResponse<Long> rentalAccept(@PathVariable Long classroomId) {
-        Long id = rentalService.rentalAccept(classroomId);
-        return ApiResponse.success("강의실 대여 승인되었습니다.", id);
-    }
-
     @Operation(
-            summary = "강의실 대여 반려 API",
-            description = "**$.detail** : (빈 문자열 불가) (100자 이내여야 함)")
-    @PatchMapping("/{classroomId}/reject")
-    public ApiResponse<Long> rentalReject(
-            @PathVariable Long classroomId,
-            @Valid @RequestBody AdminClassroomDetailRequest request) {
-        Long id = rentalService.rentalReject(classroomId, request);
-        return ApiResponse.success("강의실 대여 반려되었습니다.", id);
-    }
-
-    @Operation(summary = "강의실 반납 승인 API")
-    @PatchMapping("/{classroomId}/return")
-    public ApiResponse<Long> rentalReturn(@PathVariable Long classroomId) {
-        Long id = rentalService.rentalReturn(classroomId);
-        return ApiResponse.success("강의실 반납 되었습니다.", id);
-    }
-
-    @Operation(
-            summary = "강의실 반납시 파손 처리 API",
-            description = "**$.detail** : (빈 문자열 불가) (100자 이내여야 함)")
-    @PatchMapping("/{classroomId}/broken")
-    public ApiResponse<Long> rentalBroken(
-            @PathVariable Long classroomId,
-            @Valid @RequestBody AdminClassroomDetailRequest request) {
-        Long id = rentalService.rentalBroken(classroomId, request);
-        return ApiResponse.success("강의실 반납시 파손 처리되었습니다.", id);
-    }
-
-    @Operation(
-            summary = "강의실 대여 승인 취소 API",
-            description = "**$.detail** : (빈 문자열 불가) (100자 이내여야 함)")
-    @PatchMapping("/{classroomId}/cancel")
-    public ApiResponse<Long> rentalCancle(
-            @PathVariable Long classroomId,
-            @Valid @RequestBody AdminClassroomDetailRequest request) {
-        Long id = rentalService.rentalCancel(classroomId, request);
-        return ApiResponse.success("강의실 대여 승인 취소되었습니다.", id);
+            summary = "강의실 일괄 상태 변경 API",
+            description = """
+            - **요청 바디**
+              - **classroomIds** : 필수, 숫자 배열
+              - **status** : 필수, RETURN, REJECT, CANCEL, BROKEN, ACCEPT
+              - **detail** : 생략 가능, 100자 이내""")
+    @PatchMapping("/status")
+    public ApiResponse<List<Long>> changeStatus(@Valid @RequestBody AdminClassroomDetailRequest request) {
+        List<Long> ids = rentalService.changeStatus(request);
+        return ApiResponse.success("강의실 일괄 상태 변경되었습니다.", ids);
     }
 }
