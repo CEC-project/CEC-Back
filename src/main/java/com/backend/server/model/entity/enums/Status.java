6956c612078a401bd3b3dbd07fd42f5322003623
qkr10 <qkrtngks10@naver.com>
2025-06-03 18:39:42 +0900

    fix : 레거시 수정

--- src/main/java/com/backend/server/model/entity/enums/Status.java
+++ src/main/java/com/backend/server/model/entity/enums/Status.java
@@ -6,11 +6,12 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Status {
-    AVAILABLE("대여 가능"),
-    IN_USE("대여중"),
-    BROKEN("파손"),
-    RENTAL_PENDING("대여 신청 중"),
-    RETURN_PENDING("반납 처리중");
+    // 설명 수정. 스웨거 상에서 Enum 값이 한글로 뜸.
+    AVAILABLE("AVAILABLE(대여 가능)"),
+    IN_USE("IN_USE(대여중)"),
+    BROKEN("BROKEN(파손)"),
+    RENTAL_PENDING("RENTAL_PENDING(대여 신청 중)"),
+    RETURN_PENDING("RETURN_PENDING(반납 처리중)");
     private final String description;
 
     @Override
