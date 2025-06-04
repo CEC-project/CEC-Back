5f05af43bf91a7a50f804888df5dd7d9b5690865
klaus9267 <klaus9267@gmail.com>
2025-06-05 04:00:52 +0900

    fix: admin admin notice 수정

--- src/main/java/com/backend/server/api/common/dto/PageableInfo.java
+++ src/main/java/com/backend/server/api/common/dto/PageableInfo.java
@@ -1,5 +1,6 @@
 package com.backend.server.api.common.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.data.domain.Page;
@@ -8,10 +9,18 @@ import org.springframework.data.domain.Pageable;
 @Getter
 @AllArgsConstructor
 public class PageableInfo {
+    @Schema(description = "현재 페이지 번호", example = "1")
     private int pageNumber;
+
+    @Schema(description = "페이지 당 아이템 수", example = "1")
     private int pageSize;
+
+    @Schema(description = "전체 페이지 수", example = "1")
     private int totalPages;
+
+    @Schema(description = "전체 아이템 수", example = "1")
     private long totalElements;
+
     public PageableInfo(Page<?> page) {
         Pageable pageable = page.getPageable();
         pageNumber = pageable.getPageNumber();
