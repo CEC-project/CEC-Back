40745cd8d19de49a090f71601398e3867c4eb5ea
qkr10 <qkrtngks10@naver.com>
2025-06-10 00:00:37 +0900

    fix : 로그인시 응답값에 이름 추가, 더미데이터 문제 해결 (#287)

--- src/main/java/com/backend/server/api/common/auth/dto/CommonSignInResponse.java
+++ src/main/java/com/backend/server/api/common/auth/dto/CommonSignInResponse.java
@@ -13,6 +13,7 @@ import lombok.NoArgsConstructor;
 public class CommonSignInResponse {
     private Long id;
     private String studentNumber;
+    private String name;
     private String nickname;
     private String role;
     private String department;
@@ -24,6 +25,7 @@ public class CommonSignInResponse {
     public CommonSignInResponse(User user, String access) {
         this.id = user.getId();
         this.studentNumber = user.getStudentNumber();
+        this.name = user.getName();
         this.nickname = user.getNickname();
         this.role = user.getRole().name();
         this.department = user.getDepartment();
