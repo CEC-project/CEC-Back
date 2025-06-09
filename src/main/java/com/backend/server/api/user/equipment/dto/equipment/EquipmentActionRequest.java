cf8d735b4e44a816f7449346a72f499eb5580a66
geniusBrainLsm <87559232+geniusBrainLsm@users.noreply.github.com>
2025-06-10 07:01:35 +0900

    fix : ActionDto 수정

--- src/main/java/com/backend/server/api/user/equipment/dto/equipment/EquipmentActionRequest.java
+++ src/main/java/com/backend/server/api/user/equipment/dto/equipment/EquipmentActionRequest.java
@@ -1,13 +1,14 @@
 package com.backend.server.api.user.equipment.dto.equipment;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
-
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
 @Builder(toBuilder = true)
 public class EquipmentActionRequest {
 
