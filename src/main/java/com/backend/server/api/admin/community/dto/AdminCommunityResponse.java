8e6935d689952f7fc3b4ee13c14fadc829d92fa5
qkr10 <qkrtngks10@naver.com>
2025-06-09 14:46:25 +0900

    fix : [관리자 웹] 게시판 API 인터페이스 수정 (#278)

--- src/main/java/com/backend/server/api/admin/community/dto/AdminCommunityResponse.java
+++ src/main/java/com/backend/server/api/admin/community/dto/AdminCommunityResponse.java
@@ -1,7 +1,9 @@
 package com.backend.server.api.admin.community.dto;
 
 import com.backend.server.api.common.dto.AuthorResponse;
+import com.backend.server.api.common.dto.BoardResponse;
 import com.backend.server.model.entity.Community;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,12 +15,12 @@ public class AdminCommunityResponse {
     private Long id;
     private String title;
     private AuthorResponse author;
+    private BoardResponse board;
     private int recommend;
     private int view;
-    private Long boardCategoryId;
-    private String boardCategoryName;
     private String content;
-
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
     public AdminCommunityResponse(Community community) {
         this.id = community.getId();
@@ -26,8 +28,9 @@ public class AdminCommunityResponse {
         this.author = community.getAuthor() == null ? null : AuthorResponse.from(community.getAuthor());
         this.recommend = community.getRecommend();
         this.view = community.getView();
-        this.boardCategoryId = community.getBoardCategory().getId();
-        this.boardCategoryName = community.getBoardCategory().getName();
+        this.board = BoardResponse.from(community.getBoardCategory());
         this.content = community.getContent();
+        this.createdAt = community.getCreatedAt();
+        this.updatedAt = community.getUpdatedAt();
     }
 }
