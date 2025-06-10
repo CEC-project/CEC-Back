66d7b6a4c82866377302c16d34caf3bd8d075b48
qkr10 <qkrtngks10@naver.com>
2025-06-10 16:31:49 +0900

    fix : 강의실 관련 LocalTime 응답시, HH:mm 포맷으로 응답. (#299)
    - 학기-수업, 게시판-게시글 관계에서 부모 삭제시 자식도 삭제

--- src/main/java/com/backend/server/model/entity/classroom/Semester.java
+++ src/main/java/com/backend/server/model/entity/classroom/Semester.java
@@ -1,12 +1,17 @@
 package com.backend.server.model.entity.classroom;
 
 import com.backend.server.model.entity.BaseTimeEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -29,4 +34,7 @@ public class Semester extends BaseTimeEntity {
     private Integer year;
     private LocalDate startDate;
     private LocalDate endDate;
+
+    @OneToMany(mappedBy = "semester", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<SemesterSchedule> semesterSchedules = new ArrayList<>();
 }
