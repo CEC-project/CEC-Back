a76f79b6ae259830ca4c0f72979fc2ecbec6bd8f
geniusbrainlsm <geniusbrainlsm@gmail.com>
2025-06-14 12:06:37 +0900

    fix : BoardRepository 가 2개 있어서 빌드 안되던 오류 수정 , 문의사항 조회 시 searchType이 비어있으면 에러 발생하던 문제 해결

--- src/main/java/com/backend/server/api/admin/inquiry/dto/AdminInquiryListRequest.java
+++ src/main/java/com/backend/server/api/admin/inquiry/dto/AdminInquiryListRequest.java
@@ -24,12 +24,12 @@ public class AdminInquiryListRequest extends AbstractPaginationParam {
     }
 
     private String searchKeyword;
-    private AdminInquirySearchType searchType; // 검색 유형 (0:name|1:phoneNumber|2:studentNumber|3:nickname|4 or 생략:all)
+    private AdminInquirySearchType searchType = AdminInquirySearchType.ALL; // 검색 유형 (0:name|1:phoneNumber|2:studentNumber|3:nickname|4 or 생략:all)
     private Integer grade; // 1 or 2 or 3 or 4 or null
     private Gender gender; // '남' or '여' or null
     private Long professorId; // 교수 테이블의 id or null
     private Boolean answered; // 답변 여부 (생략시 여태까지 모든 문의가 조회됩니다.) // true or false or 생략
-    private AdminInquirySortType sortBy;
+    private AdminInquirySortType sortBy = AdminInquirySortType.CREATED_AT;
 
     public Optional<AdminUserListRequest> toAdminUserListRequest() {
         Optional<AdminUserSearchType> newSearchType = searchType.toAdminUserSearchType();
