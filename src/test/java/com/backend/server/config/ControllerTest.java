8bd6d14513224fb5650b3d3a5db0d68b68f1b54e
qkr10 <qkrtngks10@naver.com>
2025-08-01 03:53:38 +0900

    test: 테스트 격리 코드 리팩토링 + 병렬 테스트 구현 (#357)
    * fix: 기존 rate limit 기능에서, 각종 성능 저하 요소에 캐싱 추가하여 성능개선
    
    * refactor: 패키지 구조 변경 + 레디스 스크립트 관련 리팩토링
    
    1. config.annotation.rateLimit -> support.rateLimit 옮김
    2. util -> support.util 옮김
    3. RedisRateLimitRepository 의 RedisTemplate 관련 코드를 RedisScriptConfig 로 옮김
    
    * fix: 테스트 DB 격리 기능 추가
    
    * fix: develop 브랜치 추가
    
    main 브랜치는 develop 브랜치에서만 pr 가능하게 수정
    
    * refactor: 주석 추가, 각종 구현 수정
    
    1. @ControllerTest 가 @DatabaseTest 를 포함하게 함.
    2. TestDatabaseConfig.java 구현 방식 수정.
    
    * test: 병렬 테스트 기능 추가
    
    Apple M1 Pro 기준 테스트 수행시간 : 22초 -> 11초 로 11초 감소

--- src/test/java/com/backend/server/config/ControllerTest.java
+++ src/test/java/com/backend/server/config/ControllerTest.java
@@ -1,26 +1,27 @@
 package com.backend.server.config;
 
-import jakarta.transaction.Transactional;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestExecutionListeners;
 
 /**
- * 로그인을 자동으로 해주어, 컨트롤러 레이어의 테스트를 쉽게 할수 있게 해주는 어노테이션입니다.<br>
- * dev 서버용 인증필터를 사용해서, 요청에 JWT 토큰이 없어도 관리자 계정으로 자동으로 인증됩니다.<br>
+ * 실제 요청을 통한 테스트를 쉽게 작성하게 해주는 어노테이션입니다.<br>
+ * 1. 새로운 DB를 생성 / 삭제하여 테스트를 격리합니다.(테스크 클래스 마다)<br>
+ * 2. @Transactional 을 이용하여 테스트 메소드마다 DB 변경사항을 롤백합니다.<br>
+ * 3. 요청에 JWT 토큰이 없어도 관리자 계정으로 자동으로 인증됩니다.<br>
+ * 4. api rate limit 제한을 해제합니다. (분당 5회 호출 제한 등등을 해제)<br>
+ * (인증을 위해 관리자 계정을 DB에 추가합니다.)
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
-@Transactional
-@SpringBootTest
+@DatabaseTest
 @AutoConfigureMockMvc
 @ActiveProfiles("dev")
 @Import({MockMvcConfig.class, FakeRateLimitConfig.class, TestDatabaseConfig.class})
