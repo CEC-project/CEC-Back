cd78517a90c94aa297616363bf25f8e5bcc245ea
qkr10 <qkrtngks10@naver.com>
2025-08-01 11:07:54 +0900

    test: testcontainers 를 이용한 테스트 클래스별 db 격리시 30초 소요

--- src/test/java/com/backend/server/config/TestDatabaseConfig.java
+++ src/test/java/com/backend/server/config/TestDatabaseConfig.java
@@ -1,30 +1,36 @@
 package com.backend.server.config;
 
-import static com.backend.server.support.TestDatabaseUtil.PASSWORD;
-import static com.backend.server.support.TestDatabaseUtil.URL_TEMPLATE;
-import static com.backend.server.support.TestDatabaseUtil.USER;
-
-import com.backend.server.support.TestDatabaseUtil;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import javax.sql.DataSource;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @TestConfiguration
 public class TestDatabaseConfig {
+
+    private static final PostgreSQLContainer<?> POSTGRES_CONTAINER;
+
+    static {
+        POSTGRES_CONTAINER = new PostgreSQLContainer<>("postgres:15")
+                .withDatabaseName("testdb")
+                .withUsername("testuser")
+                .withPassword("testpass");
+        POSTGRES_CONTAINER.start();
+    }
 
     @Bean
     @Primary
     public DataSource dataSource() {
         HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(URL_TEMPLATE + TestDatabaseUtil.createTestDatabase());
-        config.setUsername(USER);
-        config.setPassword(PASSWORD);
-        config.setDriverClassName("org.postgresql.Driver");
+        config.setJdbcUrl(POSTGRES_CONTAINER.getJdbcUrl());
+        config.setUsername(POSTGRES_CONTAINER.getUsername());
+        config.setPassword(POSTGRES_CONTAINER.getPassword());
+        config.setDriverClassName(POSTGRES_CONTAINER.getDriverClassName());
         config.setMaximumPoolSize(5);
-        config.setPoolName("TestHikariPool");
+        config.setPoolName("TestContainerHikariPool");
 
         return new HikariDataSource(config);
     }
